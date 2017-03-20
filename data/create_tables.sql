23722d1fb8d8a2082b582d44e24f71bd7aaa8075
kylegallatin <kylegallatin@gmail.com>
2017-03-20 14:19:40 -0400

    new

---
+++ data/create_tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE photo_info
+	(caption char(255), comments float, filters char(255), hashtags char(255),  
+	image char(255), key char(255), likes float, photo_id float, post_time char(255), 
+	scrape_time char(255), tag char(255), time char(255), user char(255));
+
+CREATE TABLE user_info
+	(full_name char(255), posts float, followers float, user char(255), bio char(255),
+	following float, scrape_time char(255));
