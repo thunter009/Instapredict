8fd959e9038e3bade32d1d9e318ed08ea820123a
kylegallatin <kylegallatin@gmail.com>
2017-03-27 09:25:39 -0400

    nn

--- scrapers/instaliga/instaliga/spiders/user.py
+++ scrapers/instaliga/instaliga/spiders/user.py
@@ -9,11 +9,11 @@ import re
 class UserSpider(scrapy.Spider):
     name = "user"
     allowed_domains = ["instaliga.com"]
-<<<<<<< HEAD
-    users = pd.read_csv('../../data/users_Tues_Mar21-1.csv')['user'].unique()
-=======
-    users = pd.read_csv('../../data/Fri_Mar24_users.csv')['Fri_Mar24.user'].unique()
->>>>>>> 972e13572ba640724a3eb1a3da1e74d5f262e311
+#<<<<<<< HEAD
+    users = pd.read_csv('../../data/Sun_Mar26_users.csv')['Sun_Mar26.user'].unique()
+#=======
+ #   users = pd.read_csv('../../data/Fri_Mar24_users.csv')['Fri_Mar24.user'].unique()
+#>>>>>>> 972e13572ba640724a3eb1a3da1e74d5f262e311
     start_urls = ['https://www.instaliga.com/' + str(x) for x in users]
 
     def parse(self, response):
